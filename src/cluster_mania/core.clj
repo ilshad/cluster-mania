3f5f452fb1ea83f8b0295ccc9398ff6b49d4bb7a
Ilshad Khabibullin <astoon.net@gmail.com>
2015-02-07 22:34:21 +0300

    secrurity.clj

--- src/cluster_mania/core.clj
+++ src/cluster_mania/core.clj
@@ -1,10 +1,12 @@
 (ns cluster-mania.core
   (:require [cluster-mania.config :as config]
-            [cluster-mania.secret :as secret]
-            [cluster-mania.server :as server]))
+            [cluster-mania.server :as server]
+            [cluster-mania.security :as security]))
 
 (defn parse-message [req]
-  {:key :my-state :value 42})
+  {:key :my-state
+   :value {:ks [:a :b] :value 42}
+   :secret "c9a3dade-aec6-11e4-b809-7cd1c3f174bb"})
 
 (defn swap-fn [target]
   (case (:type target)
@@ -12,15 +14,16 @@
            (assoc-in m ks value))))
 
 (defn start [state]
-  (let [cfg (config/config)
-        nodes (config/nodes cfg)
+  (let [conf (config/config)
+        nodes (config/nodes conf)
         state (atom state)
-        secret (secret/secret)]
-    (server/start (:server-port cfg)
+        secret (security/secret)]
+    (server/start (:server-port conf)
       (fn [req]
         (let [msg (parse-message req)]
-          (if-let [target (state (:key msg))]
-            (swap! target (swap-fn target) (:value msg))))))))
+          (when (security/authorized? msg secret)
+            (when-let [target (state (:key msg))]
+              (swap! target (swap-fn target) (:value msg)))))))))
 
 (defn stop [s]
   (server/stop s))
--- src/cluster_mania/security.clj
+++ src/cluster_mania/security.clj
@@ -1,7 +1,10 @@
-(ns cluster-mania.secret
+(ns cluster-mania.security
   (:require [environ.core :as env]))
 
 (def default-secret-path "~/.cluster_mania_secret")
 
 (defn secret []
   (slurp (env/env :cluster-mania-secret-path default-secret-path)))
+
+(defn authorized? [message secret]
+  (= (:secret message) secret))
